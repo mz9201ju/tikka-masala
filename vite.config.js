109c46ce2713c17e1f27a5d15b077e417214308a
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2025-11-30 17:00:13 -0800

    basePath udpated till we buy a domain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/tikka-masala/",   // ✅ REQUIRED for GitHub Pages sub-repo
   plugins: [react()],
 })
